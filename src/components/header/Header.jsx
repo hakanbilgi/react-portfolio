6c3edb647bbb0165700d1e510fb0ae3ab333f3ac
Hakan Bilgi <hknblg34@gmail.com>
2022-11-25 17:17:34 +0300

    assets

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Header.css"
 import CTA from './CTA';
-import ME from '../../assets/me.png'
+import ME from '../../assets/me.jpg'
 import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
@@ -9,7 +9,7 @@ const Header = () => {
     
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Steve - Mentor</h1>
+        <h1>Hakan BILGI</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
         <HeaderSocial/>
