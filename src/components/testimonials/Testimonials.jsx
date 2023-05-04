751d69241c9940cf4e21374a82ecf636ebcffce2
Hakan Bilgi <hknblg34@gmail.com>
2023-05-04 17:58:31 +0300

    emailjs

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -39,25 +39,25 @@ const Testimonials = () => {
       <h2>Testimonials</h2>
 
       <Swiper className="container testimonials__container"
-          // install Swiper modules
-      modules={[Pagination]}
-      spaceBetween={40}
-      slidesPerView={1}    
-      pagination={{ clickable: true }}      
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
       >
         {
-          data.map(({ avatar, name, review}, index) => {
-            return(
+          data.map(({ avatar, name, review }, index) => {
+            return (
               <SwiperSlide className="testimonial">
-          <div className="client__avatar">
-            <img src={avatar} alt="avatar" />
-            </div>
-              <h5 className="client__name">{name}</h5>
-              <small className="client__review">{review}</small>         
-        </SwiperSlide>          
+                <div className="client__avatar">
+                  <img src={avatar} alt="avatar" />
+                </div>
+                <h5 className="client__name">{name}</h5>
+                <small className="client__review">{review}</small>
+              </SwiperSlide>
             )
           })
-        }        
+        }
       </Swiper>
     </section>
   );
