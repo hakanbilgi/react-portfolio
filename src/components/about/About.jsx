b1d0cc431f550bc5d398abc9795b819743188c59
Hakan Bilgi <hknblg34@gmail.com>
2023-08-29 00:04:18 +0300

    ...

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,7 +6,9 @@ import { FiUsers } from 'react-icons/fi'
 import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
+
   return (
+    
     <section id='about'>
       <h5>Get To Know</h5>
       <h2>About Me</h2>
