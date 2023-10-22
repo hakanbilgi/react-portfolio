8e901e5921731d4d8857307585f41c8b302e3a67
Hakan Bilgi <hknblg34@gmail.com>
2023-10-22 23:38:12 +0300

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,6 +11,8 @@ const Footer = () => {
       <a href="#" className='footer__logo'>HB</a>
       
       <ul className="permalinks">
+
+
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
@@ -18,6 +20,7 @@ const Footer = () => {
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#testimonials">Testimonials</a></li>
         <li><a href="#contact">Contact</a></li>
+        
       </ul>
 
       <div className="footer__socials">
