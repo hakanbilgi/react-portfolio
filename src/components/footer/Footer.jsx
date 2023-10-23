ea113e4fb989044d90919e5428edbe49e2c25280
Hakan Bilgi <hknblg34@gmail.com>
2023-10-23 23:46:48 +0300

    formater

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 import { FaFacebookF } from "react-icons/fa";
 import { FiInstagram } from "react-icons/fi";
 import { IoLogoTwitter } from "react-icons/io";
@@ -7,34 +7,51 @@ import { IoLogoTwitter } from "react-icons/io";
 const Footer = () => {
   return (
     <footer>
+      <a href="#" className="footer__logo">
+        HB
+      </a>
 
-      <a href="#" className='footer__logo'>HB</a>
-      
       <ul className="permalinks">
-
-
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
-        
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
       </ul>
 
       <div className="footer__socials">
-        <a href="https://www.facebook.com/developerhakan"><FaFacebookF /></a>
-        <a href="https://www.instagram.com/developerhakan/"><FiInstagram /></a>
-        <a href="https://twitter.com/developerhakan"><IoLogoTwitter /></a>
+        <a href="https://www.facebook.com/developerhakan">
+          <FaFacebookF />
+        </a>
+        <a href="https://www.instagram.com/developerhakan/">
+          <FiInstagram />
+        </a>
+        <a href="https://twitter.com/developerhakan">
+          <IoLogoTwitter />
+        </a>
       </div>
 
       <div className="footer__copyright">
         <small>&copy; Hakan BILGI.All rights reserved.</small>
       </div>
-
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
