852b01d7da86f9679f220dc79f4e500e0fb9e627
Hakan Bilgi <hknblg34@gmail.com>
2023-11-04 23:43:51 +0300

    header 3

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,9 +7,7 @@ import HeaderSocial from './HeaderSocials'
 const Header = () => {
 
   return (
-
-   
-    <div className="container header__container">
+       <div className="container header__container">
       <h5>Hello I'm</h5>
       <h1>Hakan BILGI</h1>
       <h5 className="text-light">Fullstack Developer</h5>
