7905e8a43293819562d2a4c67b87ca2f7f0cf95b
Hakan Bilgi <hknblg34@gmail.com>
2023-11-09 19:20:06 +0300

    formatter

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import './App.css';
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
-import Experience from './components/experience/Experience';
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
-import Nav from './components/nav/Nav';
-import Portfolio from './components/portfolio/Portfolio';
-import Testimonials from "./components/testimonials/Testimonials"
-import Services from './components/services/Services';
+import "./App.css";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Services from "./components/services/Services";
 
 function App() {
-  
   return (
     <>
-      <Header/>
-      <Nav/>
-      <About/>
-      <Experience/>
-      <Services/>
-      <Portfolio/>
-      <Testimonials/>
-      <Contact/>
-      <Footer/>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </>
-
-
-
   );
 }
 
