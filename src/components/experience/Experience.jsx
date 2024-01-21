6ddba7ef3737c2836f69cb9bf80cfd744e9921cd
Hakan Bilgi <hknblg34@gmail.com>
2024-01-21 17:41:56 +0300

    new 9

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,12 +4,12 @@ import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-    
 
 
 
 
-    
+
+
     <section id="experience">
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
