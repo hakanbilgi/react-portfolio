d477d21885f77fd00db84f11fd69a451903747ea
Hakan Bilgi <hknblg34@gmail.com>
2024-01-23 22:48:34 +0300

    new 10

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,15 +7,9 @@ import { IoLogoTwitter } from "react-icons/io";
 const Footer = () => {
   return (
     <footer>
-      
       <a href="#" className="footer__logo">
         HB
       </a>
-
-
-
-
-
 
       <ul className="permalinks">
         <li>
